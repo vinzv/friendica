6a54a70cb371b677cee9c53e53410ccab37e213a
Vinzenz Vietzke <vinz@vinzv.de>
2019-08-23 00:44:45 +0200

    new color scheme with gradients initial commit

---
+++ view/theme/frio/scheme/less.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Name: less
+ * Author: vinzv
+ * 
+ * List here all variables which will get overwritten through this scheme
+ * Overwrites: nav_bg, nav_icon_color, link_color, background_color, contentbg_transp
+ */
+
+	$nav_bg = "#708fa0";
+	$nav_icon_color = "#f5f5f5";
+	$link_color = "#0b486b";
+	$background_color = "#ededed";
+	$contentbg_transp = 95;
+
+
